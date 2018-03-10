a9962254a99330428996d4cf19c60836fb63e62f
Joelios <joel.mesmer@libracore.com>
2018-03-10 23:06:11 +0100

    added page background-image function

--- pagemaster/www/libra_base.py
+++ pagemaster/www/libra_base.py
@@ -2,14 +2,8 @@ from __future__ import unicode_literals
 import frappe
 
 def get_context(context):
-	#body background
 	context.bodyimage = False
 	if frappe.db.sql("SELECT value FROM tabSingles WHERE field = 'bg_select' AND doctype = 'Main Page Setup'", as_dict=True)[0].value == "Image":
 		context.bodyimage = True
 		context.bodyimagesource = frappe.db.sql("SELECT value FROM tabSingles WHERE field = 'bg_img' AND doctype = 'Main Page Setup'", as_dict=True)
-	
-	#part1 image
-	context.image = False
-	if frappe.db.sql("SELECT value FROM tabSingles WHERE field = 'layout_part1' AND doctype = 'Main Page Setup'", as_dict=True)[0].value == "Image":
-		context.image = True
-		context.imagesource = frappe.db.sql("SELECT value FROM tabSingles WHERE field = 'img_part1' AND doctype = 'Main Page Setup'", as_dict=True)
+		context.bodyimageheight = frappe.db.sql("SELECT value FROM tabSingles WHERE field = 'bg_img_height' AND doctype = 'Main Page Setup'", as_dict=True)
