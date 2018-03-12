795cc31b702d163f18f66d38d067a98f6a27939e
Joelios <joel.mesmer@libracore.com>
2018-03-12 08:44:26 +0100

    structur preparation

--- pagemaster/www/libra_base.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-import frappe
-
-def get_context(context):
-	context.bodyimage = False
-	if frappe.db.sql("SELECT value FROM tabSingles WHERE field = 'bg_select' AND doctype = 'Main Page Setup'", as_dict=True)[0].value == "Image":
-		context.bodyimage = True
-		context.bodyimagesource = frappe.db.sql("SELECT value FROM tabSingles WHERE field = 'bg_img' AND doctype = 'Main Page Setup'", as_dict=True)
-		context.bodyimageheight = frappe.db.sql("SELECT value FROM tabSingles WHERE field = 'bg_img_height' AND doctype = 'Main Page Setup'", as_dict=True)
