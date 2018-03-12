4e34b87765be36e7c08bdda1068006400c972da8
Joelios <joel.mesmer@libracore.com>
2018-03-12 14:34:12 +0100

    new setup

---
+++ pagemaster/config/pagemaster.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+    return[
+        {
+            "label": _("Main Page"),
+            "icon": "fa fa-bookmark",
+            "items": [
+                   {
+                       "type": "doctype",
+                       "name": "Main Page Setup",
+                       "label": _("Setup"),
+                       "description": _("Setup Tools for the Main Page")
+                   },
+                   {
+                       "type": "doctype",
+                       "name": "Page Slideshow",
+                       "label": _("Slideshow"),
+                       "description": _("Slideshows Setup")
+                   }
+            ]
+        },
+		{
+            "label": _("Sub Page"),
+            "icon": "fa fa-bookmark",
+            "items": []
+        }
+]
