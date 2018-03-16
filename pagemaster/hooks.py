ae8c34db0c41416444f479679e08e25612f22b4c
Joelios <joel.mesmer@libracore.com>
2018-03-16 12:07:45 +0100

    Added Function to resize the window dynamicly

--- pagemaster/hooks.py
+++ pagemaster/hooks.py
@@ -15,8 +15,8 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/pagemaster/css/pagemaster.css"
-# app_include_js = "/assets/pagemaster/js/pagemaster.js"
+# app_include_css = "/assets/pagemaster/css/pagemaster.css"
+app_include_js = "/assets/pagemaster/js/pagemaster.js"
 
 # include js, css files in header of web template
 #web_include_css = "/assets/pagemaster/css/index.css"
