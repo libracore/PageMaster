5cc87e6096a545a8629aeaf2918d3ac09f3e3b3c
Joelios <joel.mesmer@libracore.com>
2018-03-21 16:11:06 +0100

    preparing feature #10

--- pagemaster/www/news.py
+++ pagemaster/www/news.py
@@ -42,6 +42,4 @@ def get_context(context):
 	context.categories = frappe.get_all('Blog Category', filters={'published': '1'}, fields=['title'])
 	
 	#blog posts
-	#context.posts = frappe.get_all('Blog Post', filters={'published': '1'}, fields=['title', 'blog_category', 'blogger', 'published_on', 'blog_intro'])
-	context.posts = frappe.get_list('Blog Post', fields=['title', 'blog_category', 'blogger', 'published_on', 'blog_intro'], filters={'published': '1'}, order_by='published_on desc', limit_page_length=None, ignore_permissions=True)
-	#context.older_posts = frappe.get_list('Blog Post', fields=['title', 'blog_category', 'blogger', 'published_on', 'blog_intro'], filters={'published': '1'}, order_by='published_on', limit_start=3, ignore_permissions=True)
+	context.posts = frappe.get_list('Blog Post', fields=['title', 'blog_category', 'blogger', 'published_on', 'blog_intro', 'prev_img'], filters={'published': '1'}, order_by='published_on desc', limit_page_length=None, ignore_permissions=True)
